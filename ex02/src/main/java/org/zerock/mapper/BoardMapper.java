9115615b6b98da1fe1c30747ffadf820318c43f5
leejeongchan <dlwjdcks5343@naver.com>
2019-03-13 21:17:10 +0900

    CRUD 마이바티스 임시 테스트 구현

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -1,10 +1,30 @@
 package org.zerock.mapper;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Select;
-import org.zerock.domain.BoardVO;
 
+import org.zerock.domain.BoardVO;
+/*
+ * 마이바티스 인터페이스 게시글 관련
+ *
+ *
+ * getList(): 게시글 테이블 모든 정보 
+ * insert(BoardVO board): 게시글 테이블에 삽입
+ * insertSelectKey(BoardVO board): 게시글 테이블에 삽입 하돼 시퀀스 넘버를 알고 삽입
+ * read(Long bno): 게시글 번호로 조회
+ * delete(Long bno): 게시글 번호로 해당 게시글 삭제
+ * update(BoardVO board): 해당 게시글 수정
+ */
 public interface BoardMapper {
-	//@Select("select * from tbl_board where bno>0")
+	
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
 }
