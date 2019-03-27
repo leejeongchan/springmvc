2aac3bfa3af4d91fd6d738b3de40ddc925db9bd0
leejeongchan <dlwjdcks5343@naver.com>
2019-03-27 18:03:01 +0900

    검색 처리 완료 + 각 페이지에서 검색 조건 유지 + UriComponentsBuilder사용

--- ex02/src/main/java/org/zerock/domain/Criteria.java
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -1,5 +1,7 @@
 package org.zerock.domain;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -26,5 +28,15 @@ public class Criteria {
 	//검색 조건이 (T,W,C)로 구성되어 검색 조건을 배열로 만듬 Mybatis 동적 태그 활용
 	public String[] getTypeArr() {
 		return type == null ? new String[] {}: type.split("");
+	}
+	
+	//UriCOmponentsBuilder 여러개의 파라미터를 연결해서 하나의 URL을 제공
+	public String getListLink() {
+		UriComponentsBuilder builder= UriComponentsBuilder.fromPath("")
+				.queryParam("pageNum", this.pageNum)
+				.queryParam("amount", this.getAmount())
+				.queryParam("type", this.getType())
+				.queryParam("keyword", this.getKeyword());
+		return builder.toUriString();
 	}
 }
